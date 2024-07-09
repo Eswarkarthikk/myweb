4ff91ae4d208c8cc5cc9eb8a8a51461361ee179e
Eswarkarthikk <101045468+Eswarkarthikk@users.noreply.github.com>
2024-07-09 12:18:19 +0530

    Update settings.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$3q248c8aa=v*uttprq@9p)s&=-o-6+40$d9!bucd-_jaa-p23
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myresume.vercel.app', '*']
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 
 # Application definition
