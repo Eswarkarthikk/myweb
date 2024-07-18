f2be22bde98a5abfb5518aa0af60e4f22640d7c4
Eswarkarthikk <101045468+Eswarkarthikk@users.noreply.github.com>
2024-07-18 23:32:31 +0530

    Update views.py

--- myresume/views.py
+++ myresume/views.py
@@ -1,34 +1,31 @@
-import io
-import base64
 import os
 import shutil
+import base64
+import tempfile
 from django.shortcuts import render
 from django.http import HttpResponseServerError
 from .forms import UploadImageForm
-from gradio_client import Client
+from gradio_client import Client, handle_file
 from PIL import Image
+import httpx
 
 # Initialize the Gradio client with the correct endpoint URL
 client = Client("https://eswarkarthikk-object-detection.hf.space")
 
-def convert_image_to_rgb(image):
-    """Convert RGBA image to RGB."""
-    if image.mode == 'RGBA':
-        return image.convert('RGB')
-    return image
-
-def compress_image(image, max_bytes=2 * 1024 * 1024):
+def compress_image(image_file, max_bytes=2 * 1024 * 1024):
     """Compress image to fit within the specified byte size."""
-    img = Image.open(image)
-    img = convert_image_to_rgb(img)
-    buffer = io.BytesIO()
+    img = Image.open(image_file)
+    
+    # Ensure the image is in RGB format
+    img = img.convert('RGB')
+    
+    buffer = tempfile.SpooledTemporaryFile(max_size=max_bytes)
     
     # Save the image with reduced quality to fit within the byte limit
     img.save(buffer, format='JPEG', quality=85)
     
-    # Ensure the image fits within the byte size limit
-    while buffer.tell() > max_bytes:
-        img.save(buffer, format='JPEG', quality=75)
+    # Rewind the buffer to the beginning
+    buffer.seek(0)
     
     return buffer
 
@@ -44,10 +41,11 @@ def upload_image(request):
                 compressed_image_buffer = compress_image(image_file)
 
                 # Convert the original image to base64
-                compressed_image_base64 = base64.b64encode(compressed_image_buffer.getvalue()).decode('utf-8')
+                original_image_base64 = base64.b64encode(image_file.read()).decode('utf-8')
 
-                # Send the base64 encoded image to the Gradio API
-                result = client.predict(image=compressed_image_base64)
+                # Example using gradio_client to predict with local file
+                with httpx.Client(timeout=30) as http_client:  # Set a longer timeout if needed
+                    result = client.predict(image=handle_file(compressed_image_buffer))
 
                 # Check if the result is a file path
                 if isinstance(result, str) and os.path.isfile(result):
@@ -57,7 +55,7 @@ def upload_image(request):
 
                     # Pass the base64 encoded images to the template
                     return render(request, 'index.html', {
-                        'original_image_base64': compressed_image_base64,
+                        'original_image_base64': original_image_base64,
                         'predicted_image_base64': predicted_image_base64
                     })
                 else:
