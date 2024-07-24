4a96d6683c8593278bfc42813fbc4b593ec05b43
Eswarkarthikk 😎😎 <eswarkarthikk595@gmail.com>
2024-07-24 14:47:14 +0530

    updated again

--- myresume/urls.py
+++ myresume/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    path('',views.mainpage,name='mainpage'), 
-    path('upload_image',views.upload_image,name='upload_image')
+    #path('',views.mainpage,name='mainpage'), 
+    path('',views.upload_image,name='upload_image')
 ]
 
